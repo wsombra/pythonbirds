2453e1c8d6d7a4a1d72267dbb50deaf14ac93816
wsombra <washingtonfreitass@hotmail.com>
2020-03-31 01:50:40 -0300

    Criado o atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,12 @@
 class Pessoa:
     # pra criar os atributos utilizo os método especial __init_(self)
-                      # parametro
-     def __init__(self, nome=None, idade=40):
-         # Atributo nome , passando um none type
+                   # * QTD não definida |  parametros
+     def __init__(self, *filhos,  nome=None, idade=40):
+         # Atributos
          self.idade = idade
          self.nome  = nome
+         # Atibuto filho recebe um list
+         self.filhos = list(filhos)
 
 
      def cumprimentar(self):
@@ -19,4 +21,10 @@ if __name__ == '__main__':
         g = Pessoa('Freitas', 40)
         print(g.nome)
         print(g.idade)
-
+''' Trabalhanddo com atributos complexos '''
+cadu = Pessoa(nome='Carlos', idade=5)
+raildo = Pessoa(nome='Raildo')
+sombra = Pessoa(cadu, raildo, nome='Sombra', idade=40) #recebendo objetos...
+print(sombra.nome)
+for filho in sombra.filhos:
+    print(filho.nome)
