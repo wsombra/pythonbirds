ce33a7a1b5cc878c8f72aa82f7052a6a6deaaa26
wsombra <washingtonfreitass@hotmail.com>
2020-04-01 16:54:23 -0300

    Criado os métodos de classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,7 +3,7 @@ class Pessoa:
     olhos = 2
     ''' 
     - Atributo de "classe", pois é um atributo comum entre os demais objetos (essa é a crítica)
-    - Para criar os atributos utilizo os método especial __init_(self)
+    - Para criar os atributos utilizo o método especial __init_(self)
     - * QTD não definida |  parametros'''
     def __init__(self, *filhos,  nome=None, idade=40):
         # Atributos
@@ -12,9 +12,23 @@ class Pessoa:
         # Atibuto filho recebe um list
         self.filhos = list(filhos)
 
-
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    '''
+    Método de classe , iniciado com @, temos dois tipos : static e o class
+    Exemplos:
+    '''
+    @staticmethod
+    def calcula_imc():
+        return 10
+
+    ''' Ter acesso a classe que está executando o método'''
+    @classmethod
+    def nome_e_atrib_de_classe(cls):
+        '''Retornando a classe  e os atributos'''
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
         p = Pessoa()
         print(p.cumprimentar())
@@ -24,11 +38,11 @@ if __name__ == '__main__':
         g = Pessoa('Freitas', 40)
         print(g.nome)
         print(g.idade)
-''' Trabalhanddo com atributos complexos '''
-cadu = Pessoa(nome='Carlos', idade=5)
-raildo = Pessoa(nome='Raildo')
-sombra = Pessoa(cadu, raildo, nome='Sombra', idade=40) #recebendo objetos...
-print(sombra.nome)
+        ''' Trabalhanddo com atributos complexos '''
+        cadu = Pessoa(nome='Carlos', idade=5)
+        raildo = Pessoa(nome='Raildo')
+        sombra = Pessoa(cadu, raildo, nome='Sombra', idade=40) #recebendo objetos...
+        print(sombra.nome)
 for filho in sombra.filhos:
     print(filho.nome)
     # python permite criar atributos em tempo de execução, exemplo !!!! não é boa prática!!!
@@ -40,3 +54,5 @@ for filho in sombra.filhos:
     # del cadu.sobrenome
     # Objeto cadu acessando o atributo de classe.
     print(cadu.olhos)
+    # Acesso ao meétodo via classe e objeto:
+    print(Pessoa.nome_e_atrib_de_classe(), cadu.nome_e_atrib_de_classe())
